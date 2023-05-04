19c6be8485fdc361c5c3bf01d90d4747c78fe584
ahayworth1 <adamhayworth@unomaha.edu>
2023-05-04 10:04:19 -0500

    Initial commit

---
+++ quasar-project/src/router/routes.js
@@ -0,0 +1,17 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('src/pages/PageHome.vue') },
+      { path: '/about', component: () => import('src/pages/PageAbout.vue') }
+
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+
+]
+
+export default routes
